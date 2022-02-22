d65b7797db59302c63517a53657aeeb1a762d3f7
nckim <qazqaz1000@hanmail.net>
2022-02-23 00:14:10 +0900

    movie adapter 추가

---
+++ presentation/src/main/java/com/nckim/cleanarchstudy/views/search/MovieAdapter.kt
@@ -0,0 +1,56 @@
+package com.nckim.cleanarchstudy.views.search
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.nckim.cleanarchstudy.R
+import com.nckim.cleanarchstudy.databinding.RecyclerMovieItemBinding
+import com.nckim.domain.model.search.Movie
+
+class MovieAdapter(private val itemClick: (Movie) -> Unit) :
+    ListAdapter<Movie, MovieAdapter.ViewHolder>(
+        diffUtil
+    ) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding: RecyclerMovieItemBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.recycler_movie_item,
+            parent,
+            false
+        )
+        return ViewHolder(binding).apply {
+            binding.root.setOnClickListener { view ->
+                val position = adapterPosition.takeIf { it != RecyclerView.NO_POSITION }
+                    ?: return@setOnClickListener
+                itemClick(getItem(position))
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class ViewHolder(private val binding: RecyclerMovieItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(movie: Movie) {
+            binding.movie = movie
+            binding.executePendingBindings()
+        }
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<Movie>() {
+            override fun areContentsTheSame(oldItem: Movie, newItem: Movie) =
+                oldItem == newItem
+
+            override fun areItemsTheSame(oldItem: Movie, newItem: Movie) =
+                oldItem.title == newItem.title
+        }
+    }
+
+}
