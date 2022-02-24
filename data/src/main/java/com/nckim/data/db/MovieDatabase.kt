bef26a0cae44a95c66b97a2f428caf408585b463
nckim <nckim@forcs.com>
2022-02-24 10:19:05 +0900

    누락코드 추가

--- data/src/main/java/com/nckim/data/db/MovieDatabase.kt
+++ data/src/main/java/com/nckim/data/db/MovieDatabase.kt
@@ -1,9 +1,10 @@
 package com.nckim.data.db
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.nckim.data.model.search.MovieEntity
 
 @Database(entities = [MovieEntity::class], version = 1, exportSchema = false)
-abstract class MovieDatabase {
+abstract class MovieDatabase :RoomDatabase(){
     abstract fun movieDao(): MovieDao
 }
